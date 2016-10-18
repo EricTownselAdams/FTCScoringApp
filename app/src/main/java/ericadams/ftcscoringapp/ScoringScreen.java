3239501ec828fbf98be2d929ff7da7a17727d6ec
Eric <smadagotu@gmail.com>
2016-10-18 19:43:39 -0400

    Working drop down menu
    This commit including a working dropdown menu.  However, it does not do
    anything on selection

--- app/src/main/java/ericadams/ftcscoringapp/ScoringScreen.java
+++ app/src/main/java/ericadams/ftcscoringapp/ScoringScreen.java
@@ -9,7 +9,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-public class ScoringScreen extends AppCompatActivity {
+public class ScoringScreen extends AppCompatActivity  {
     Spinner CapBallSpinA;
     ArrayAdapter<CharSequence> CapBallAdapterAA;
 
@@ -17,14 +17,18 @@ public class ScoringScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoring_screen);
-        CapBallSpinA = (Spinner)findViewById(R.id.spinner);
+        CapBallSpinA = (Spinner)findViewById(R.id.CapBallSpinA);
         CapBallAdapterAA = ArrayAdapter.createFromResource(this,R.array.CapBallAOptions,android.R.layout.simple_spinner_item);
         CapBallAdapterAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         CapBallSpinA.setAdapter(CapBallAdapterAA);
-        CapBallSpinA.setOnItemSelectedListener(new AdapterView.onItemSelectedListener(this) {
+        CapBallSpinA.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+              //  Toast.makeText()(getBaseContext(),parent.getItemAtPosition(,position)+"selected",Toast.LENGTH_LONG).show();
+            }
 
-            public void OnItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+"selected",Toast.LENGTH_LONG);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
