e0aac7cf14091912c6f1e2c3df00fef26e260030
Eric <smadagotu@gmail.com>
2016-10-25 20:00:40 -0400

    Added two more spinners, one still not working

--- app/src/main/java/ericadams/ftcscoringapp/ScoringScreen.java
+++ app/src/main/java/ericadams/ftcscoringapp/ScoringScreen.java
@@ -11,28 +11,32 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
+import static ericadams.ftcscoringapp.R.id.BeaconScoreA;
 import static ericadams.ftcscoringapp.R.id.CapBallScoreA;
+import static ericadams.ftcscoringapp.R.id.ParkedSpinnerA;
 
 public class ScoringScreen extends AppCompatActivity  {
-    Spinner CapBallSpinA;
+    Spinner CapBallSpinnerA;
     ArrayAdapter<CharSequence> CapBallAdapterAA;
+    Spinner BeaconSpinnerA;
+    ArrayAdapter<CharSequence> BeaconAdapterA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // final TextView CapBallScoreA = (TextView) findViewById(R.id.CapBallScoreA);
         setContentView(R.layout.activity_scoring_screen);
-        CapBallSpinA = (Spinner)findViewById(R.id.CapBallSpinA);
+        CapBallSpinnerA = (Spinner)findViewById(R.id.CapBallSpinA);//Here --------------------------
         CapBallAdapterAA = ArrayAdapter.createFromResource(this,R.array.CapBallAOptions,android.R.layout.simple_spinner_item);
         CapBallAdapterAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        CapBallSpinA.setAdapter(CapBallAdapterAA);
-        CapBallSpinA.setOnItemSelectedListener(new OnItemSelectedListener() {
+        CapBallSpinnerA.setAdapter(CapBallAdapterAA);
+
+        CapBallSpinnerA.setOnItemSelectedListener(new OnItemSelectedListener() {
             final TextView CapBallScoreA = (TextView) findViewById(R.id.CapBallScoreA);
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("thing",CapBallSpinA.getSelectedItem().toString());
+                Log.d("thing",CapBallSpinnerA.getSelectedItem().toString());
 
-                if (CapBallSpinA.getSelectedItem().toString().equals("On Floor")){
+                if (CapBallSpinnerA.getSelectedItem().toString().equals("On Floor")){
                     CapBallScoreA.setText("5");
                 } else{
                     CapBallScoreA.setText("0");
@@ -42,9 +46,79 @@ public class ScoringScreen extends AppCompatActivity  {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-              //  int CapBallScoreA = 0;
             }
         });
-    //    Log.d(CapBallSpinA.getSelectedItem().toString());
+
+        BeaconSpinnerA = (Spinner)findViewById(R.id.BeaconSpinnerA);//Here --------------------------
+        BeaconAdapterA = ArrayAdapter.createFromResource(this,R.array.BeaconAOptions,android.R.layout.simple_spinner_item);
+        BeaconAdapterA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        BeaconSpinnerA.setAdapter(BeaconAdapterA);
+        BeaconSpinnerA.setOnItemSelectedListener(new OnItemSelectedListener() {
+            final TextView BeaconScoreA = (TextView) findViewById(R.id.BeaconScoreA);
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("thing",BeaconSpinnerA.getSelectedItem().toString());
+
+                if (BeaconSpinnerA.getSelectedItem().toString().equals("1 Beacon")){
+                    BeaconScoreA.setText("30");
+                } else if(BeaconSpinnerA.getSelectedItem().toString().equals("2 Beacons")){
+                    BeaconScoreA.setText("60");
+                } else if(BeaconSpinnerA.getSelectedItem().toString().equals("0 Beacons")){
+                    BeaconScoreA.setText("0");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });// Here--------------------------------------------------------------------------------
+        ParkedSpinnerA = (Spinner)findViewById(ParkedSpinnerA);//Here --------------------------
+        BeaconAdapterA = ArrayAdapter.createFromResource(this,R.array.ParkedOptions ,android.R.layout.simple_spinner_item);
+        ParkedSpinnerA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ParkedSpinnerA (CHANGE );
+
+        CHANGE .setOnItemSelectedListener(new OnItemSelectedListener() {
+            final TextView CHANGE  = (TextView) findViewById(R.id.CHANGE );
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("thing",CHANGE .getSelectedItem().toString());
+
+                if (CHANGE .getSelectedItem().toString().equals("On Floor")){
+                    CHANGE .setText("CHANGE");
+                } else{
+                    CHANGE .setText("CHANGE");
+                    //txtValue.setText(Integer.toString(0));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 }
+/*
+ CHANGE = (Spinner)findViewById(CHANGE);//Here --------------------------
+        CHANGE = ArrayAdapter.createFromResource(this,R.array.CHANGE ,android.R.layout.simple_spinner_item);
+        CHANGE (android.R.layout.simple_spinner_dropdown_item);
+        CHANGE (CHANGE );
+
+        CHANGE .setOnItemSelectedListener(new OnItemSelectedListener() {
+            final TextView CHANGE  = (TextView) findViewById(R.id.CHANGE );
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("thing",CHANGE .getSelectedItem().toString());
+
+                if (CHANGE .getSelectedItem().toString().equals("On Floor")){
+                    CHANGE .setText("CHANGE");
+                } else{
+                    CHANGE .setText("CHANGE");
+                    //txtValue.setText(Integer.toString(0));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+*/
