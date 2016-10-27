51160f5b42508882bbfc559e1a7374745531f356
Eric <smadagotu@gmail.com>
2016-10-27 19:55:11 -0400

    Including Robot Parked spinner
    This commit inculdes the robot parked spinner working.  This also
    includes the foundation for the center goal balls.  That means it has a
    plus and minus button, however, they are not working.

--- app/src/main/java/ericadams/ftcscoringapp/ScoringScreen.java
+++ app/src/main/java/ericadams/ftcscoringapp/ScoringScreen.java
@@ -11,24 +11,34 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-import static ericadams.ftcscoringapp.R.id.BeaconScoreA;
-import static ericadams.ftcscoringapp.R.id.CapBallScoreA;
-import static ericadams.ftcscoringapp.R.id.ParkedSpinnerA;
 
 public class ScoringScreen extends AppCompatActivity  {
     Spinner CapBallSpinnerA;
     ArrayAdapter<CharSequence> CapBallAdapterAA;
     Spinner BeaconSpinnerA;
     ArrayAdapter<CharSequence> BeaconAdapterA;
+    Spinner ParkedSpinnerA;
+    ArrayAdapter<CharSequence> ParkedAdapterA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoring_screen);
+
         CapBallSpinnerA = (Spinner)findViewById(R.id.CapBallSpinA);//Here --------------------------
         CapBallAdapterAA = ArrayAdapter.createFromResource(this,R.array.CapBallAOptions,android.R.layout.simple_spinner_item);
         CapBallAdapterAA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         CapBallSpinnerA.setAdapter(CapBallAdapterAA);
+
+        BeaconSpinnerA = (Spinner)findViewById(R.id.BeaconSpinnerA);//Here --------------------------
+        BeaconAdapterA = ArrayAdapter.createFromResource(this,R.array.BeaconAOptions,android.R.layout.simple_spinner_item);
+        BeaconAdapterA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        BeaconSpinnerA.setAdapter(BeaconAdapterA);
+
+        ParkedSpinnerA = (Spinner)findViewById(R.id.ParkedSpinnerA);
+        ParkedAdapterA = ArrayAdapter.createFromResource(this,R.array.ParkedOptions,android.R.layout.simple_spinner_item);
+        ParkedAdapterA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ParkedSpinnerA.setAdapter(ParkedAdapterA);
 
         CapBallSpinnerA.setOnItemSelectedListener(new OnItemSelectedListener() {
             final TextView CapBallScoreA = (TextView) findViewById(R.id.CapBallScoreA);
@@ -49,10 +59,7 @@ public class ScoringScreen extends AppCompatActivity  {
             }
         });
 
-        BeaconSpinnerA = (Spinner)findViewById(R.id.BeaconSpinnerA);//Here --------------------------
-        BeaconAdapterA = ArrayAdapter.createFromResource(this,R.array.BeaconAOptions,android.R.layout.simple_spinner_item);
-        BeaconAdapterA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        BeaconSpinnerA.setAdapter(BeaconAdapterA);
+
         BeaconSpinnerA.setOnItemSelectedListener(new OnItemSelectedListener() {
             final TextView BeaconScoreA = (TextView) findViewById(R.id.BeaconScoreA);
             @Override
@@ -71,23 +78,27 @@ public class ScoringScreen extends AppCompatActivity  {
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
-        });// Here--------------------------------------------------------------------------------
-        ParkedSpinnerA = (Spinner)findViewById(ParkedSpinnerA);//Here --------------------------
-        BeaconAdapterA = ArrayAdapter.createFromResource(this,R.array.ParkedOptions ,android.R.layout.simple_spinner_item);
-        ParkedSpinnerA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ParkedSpinnerA (CHANGE );
+        });
 
-        CHANGE .setOnItemSelectedListener(new OnItemSelectedListener() {
-            final TextView CHANGE  = (TextView) findViewById(R.id.CHANGE );
+        ParkedSpinnerA.setOnItemSelectedListener(new OnItemSelectedListener() {
+            final TextView ParkedScoreA  = (TextView) findViewById(R.id.ParkedScoreA);
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("thing",CHANGE .getSelectedItem().toString());
+                Log.d("thing",ParkedSpinnerA .getSelectedItem().toString());
 
-                if (CHANGE .getSelectedItem().toString().equals("On Floor")){
-                    CHANGE .setText("CHANGE");
-                } else{
-                    CHANGE .setText("CHANGE");
+                if (ParkedSpinnerA.getSelectedItem().toString().equals("Half Center")){
+                    ParkedScoreA.setText("5");
+                } else if(ParkedSpinnerA.getSelectedItem().toString().equals("Full Center")){
+                    ParkedScoreA.setText("10");
                     //txtValue.setText(Integer.toString(0));
+                } else if(ParkedSpinnerA.getSelectedItem().toString().equals("Half Corner")){
+                    ParkedScoreA.setText("5");
+                    //txtValue.setText(Integer.toString(0));
+                } else if(ParkedSpinnerA.getSelectedItem().toString().equals("Full Corner")){
+                    ParkedScoreA.setText("10");
+                    //txtValue.setText(Integer.toString(0));
+                } else if(ParkedSpinnerA.getSelectedItem().toString().equals("Not Parked")){
+                    ParkedScoreA.setText("0");
                 }
             }
 
